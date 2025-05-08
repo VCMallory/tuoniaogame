69e31e8902b1aeac2821a3361dd1d7d2c35c2041
YAOTAO <31396502+VCMallory@users.noreply.github.com>
2025-05-08 08:02:01 +0800

    调整

--- temp/quick-scripts/src/__qc_index__.js
+++ temp/quick-scripts/src/__qc_index__.js
@@ -1,6 +1,4 @@
 
-require('./assets/scripts/index0/index0');
-require('./assets/scripts/index1/AgreementCtrl');
 require('./assets/scripts/index2/colorfonts');
 require('./assets/scripts/index2/retu');
 require('./assets/scripts/loadtextanimation');
